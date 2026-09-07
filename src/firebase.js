585d05fbc16c3a6a1e787ff3b513992f77fac477
christdembele00-droid <christdembele00@gmail.com>
2026-09-07 01:30:25 +0000

    Connect Winerland Firebase Realtime Database

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
 import { getApps, initializeApp } from 'firebase/app';
 import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || 'winerland-8844f',
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  databaseURL:
+    process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL ||
+    'https://winerland-8844f-default-rtdb.firebaseio.com',
 };
 
 export const firebaseConfigured = Boolean(
@@ -29,3 +33,4 @@ try {
 
 export { auth };
 export const db = getFirestore(app);
+export const realtimeDb = getDatabase(app);
